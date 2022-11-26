88233a90b8bf241aaa8161b3a60206e6b4cb3fc7
Kaio Macedo <kaiomm2000@gmail.com>
2022-11-25 23:36:01 -0300

    1.01

---
+++ Store-KJ/src/main/java/br/unitins/kj/controller/IndexController.java
@@ -0,0 +1,19 @@
+package br.unitins.kj.controller;
+
+import java.io.Serializable;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+@Named
+@ViewScoped
+public class IndexController implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8698636463676176036L;
+
+	
+	
+}
---
+++ Store-KJ/src/main/java/br/unitins/kj/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package br.unitins.kj.repository;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import br.unitins.kj.model.Usuario;
+
+@ApplicationScoped
+public class UsuarioRepository extends Repository<Usuario>{
+
+}
